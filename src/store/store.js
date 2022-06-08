49bc267c9fe096ba09e452fbcdab784d5244c84d
sdntmn <67434735+sdntmn@users.noreply.github.com>
2022-06-09 00:26:16 +0500

    fix
    Почистил

--- src/store/store.js
+++ src/store/store.js
@@ -2,26 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "./rootReducer";
 import thunk from "redux-thunk";
 
-// function saveToLocalStorage(state) {
-//   try {
-//     const serialisedState = JSON.stringify(state.favoriteCards);
-//     localStorage.setItem("favoriteCards", serialisedState);
-//   } catch (e) {
-//     console.warn(e);
-//   }
-// }
-
-// function loadFromLocalStorage() {
-//   try {
-//     const serialisedState = localStorage.getItem("favoriteCards");
-//     if (serialisedState === null) return undefined;
-//     return JSON.parse(serialisedState);
-//   } catch (e) {
-//     console.warn(e);
-//     return undefined;
-//   }
-// }
-
 const loadLocal = () => {
   try {
     const savedState = localStorage.getItem("saveCard");
